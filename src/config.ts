7884411b2593f6a63e21359896a8ed00181a90ec
Javi Mont <jmonterohoyos@gmail.com>
2025-03-14 21:38:53 +0100

    change: config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://jvmnt.pages.dev/", // replace this with your deployed domain
+  author: "Javier Montero",
+  profile: "null",
+  desc: "Blog personal.",
+  title: "jvmnt",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
